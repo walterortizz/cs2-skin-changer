78794c14af192d077b1f2ea35d32350c7a39bad3
Walter Ortiz <walterortizdev@gmail.com>
2025-02-16 17:35:35 -0300

    first commit

---
+++ src/config.js
@@ -0,0 +1,19 @@
+exports.config = {
+  name: "Waltercito Server",
+  lang: "en",
+  DB: {
+    DB_HOST: process.env.DB_HOST,
+    DB_USER: process.env.DB_USER,
+    DB_PASS: process.env.DB_PASS,
+    DB_DB: process.env.DB_DB,
+    DB_PORT: process.env.DB_PORT,
+  },
+  HOST: process.env.HOST,
+  PROTOCOL: process.env.PROTOCOL,
+  PORT: process.env.PORT,
+  STEAMAPIKEY: process.env.STEAMAPIKEY,
+  connect: {
+    show: true,
+    url: "steam://connect/15.228.0.15:27015?appid=730/",
+  },
+};
